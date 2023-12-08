434ec8774f84c680973b08ca85ee8b2f6bf8a4f3
sanketvalvi <valvisanket7887@gmail.com>
2023-12-08 13:53:02 +0530

    feature1

--- Hello.java
+++ Hello.java
@@ -1,8 +1,15 @@
 public class Hello
 {
+
 	public static void main(String args[])
 	{
 		System.out.println("Hello Git");
 		System.out.println("Dev 2");
+
+		
+	public void feature1()
+	{
+		System.out.println("Feature 1");
+	}
 	}
 }
